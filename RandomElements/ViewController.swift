c38ac8b16aaacfad16007b1fcadd71f68dc785e3
Anton Akhmedzyanov <anton.akhmedzyanov@gmail.com>
2023-07-30 23:17:50 +0300

    moreComit

--- RandomElements/ViewController.swift
+++ RandomElements/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var segmentedCNR: UISegmentedControl!
+    @IBOutlet var mainTextLabel: UILabel!
+    
+    private var indexNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTextLabel()
+        
+        segmentedCNR.insertSegment(withTitle: "Third", at: 2, animated: false)
     }
 
 
+    @IBAction func changeSigment() {
+       
+        
+    }
+    
+    private func setupTextLabel() {
+        mainTextLabel.text = ""
+        mainTextLabel.font = mainTextLabel.font.withSize(35)
+        mainTextLabel.textAlignment = .center
+        mainTextLabel.numberOfLines = 2
+        mainTextLabel.textColor = .brown
+    }
 }
 
